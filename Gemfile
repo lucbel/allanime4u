3f8649169e8c188a59d92f475e9086d90f131eb1
Rodrigo Duran Rodriguez <rodrigo@moifi.com>
2016-03-17 14:24:34 +0100

    add a test to connect with mongo

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # database mongodb
-gem 'mongo', '~> 2.1'
+gem 'mongo', '~> 2.2'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- test/helpers/tc_template_repository.rb
+++ test/helpers/tc_template_repository.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
+require 'mongo'
 
 class TemplateRepositoryTest < ActiveSupport::TestCase
 
   test "create template repository" do
     assert_not_nil(TemplateRepository::new)
+  end
+
+  test "create mongo db connection" do
+    db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'test')
+    assert_not_nil(db)
+    db.use("test")
+    coll = db [:example]
+    assert_not_nil(coll)
+    coll.insert_one({"name"=>"second example"})
   end
 
 end
